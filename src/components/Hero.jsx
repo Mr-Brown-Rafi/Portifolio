57a43c8afc70cce20fee172086f3781b9ff14aa4
Brown Boy Rafi <rafidmr0438@gmail.com>
2024-11-21 16:34:59 +0530

    divided hero section and worked on css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../styles/Hero.css';
+import Dp from "../assets/Portfolio_pic-transformed.jpeg"
 
 function Hero() {
   return (
-    <section className="hero" id='hero'>
-      <div className="hero-content">
-        <h1>Hello, I'm Rafi</h1>
-        <p>
-        Full Stack Developer with 4 years of experience designing, developing, and deploying scalable web applications. Proficient in .NET Core, React, and cloud technologies with expertise in building end-to-end solutions. Certified Azure Developer Associate, adept at migrating legacy systems to modern architectures and optimizing performance. Passionate about exploring microservices and distributed systems to deliver high-quality user experiences.
+<section id="hero" className="hero">
+      <div className="hero-left">
+        <img
+          src={Dp}
+          alt="Profile"
+          className="hero-photo"
+        />
+      </div>
+      <div className="hero-right">
+        <h1 className="hero-title">Welcome to My Portfolio</h1>
+        <p className="hero-quote">
+          "Success is not final, failure is not fatal: It is the courage to continue that counts."
         </p>
-        <button className="cta">Learn More</button>
+        <p className="hero-liner">.NET and React Developer | DevOps Enthusiast</p>
       </div>
     </section>
   );
